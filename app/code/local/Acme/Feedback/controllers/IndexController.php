540b98ffb478124263e164931914141425fa7f94
stas <potapenko.stanislav@gmail.com>
2016-08-26 17:50:11 +0300

    Added Acme Feedback index action

---
+++ app/code/local/Acme/Feedback/controllers/IndexController.php
@@ -0,0 +1,18 @@
+ <?php
+ /**
+ * News frontend controller
+ *
+ * @author Magento
+ */
+class Acme_Feedback_IndexController extends Mage_Core_Controller_Front_Action
+{
+    
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+        var_dump('Stas test!');
+
+    }
+}
